45621a18d3fb30d8fd961835ed42cdd23bc386fb
William R. Fraser <wfraser@codewise.org>
2015-08-14 10:53:51 -0700

    add basic definition of StackMemoryNode

---
+++ TIS-100 Simulator/StackMemoryNode.cpp
@@ -0,0 +1,5 @@
+#include "pch.h"
+#include "Node.h"
+#include "StackMemoryNode.h"
+#include "IOChannel.h"
+
---
+++ TIS-100 Simulator/StackMemoryNode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class StackMemoryNode : public INode
+{
+private:
+    std::vector<int> m_data;
+
+public:
+    StackMemoryNode();
+    virtual void SetNeighbor(Neighbor direction, std::shared_ptr<IOChannel>& spIO);
+    virtual void Initialize();
+
+    virtual void Read();
+    virtual void Compute();
+    virtual void Write();
+    virtual void WriteComplete();
+    virtual void Step();
+};
