5d17c6ec99cc7cc6e989a06c91058fa780a0b24b
William R. Fraser <wfraser@codewise.org>
2015-08-14 12:54:12 -0700

    StackMemoryNode stubs

--- TIS-100 Simulator/StackMemoryNode.cpp
+++ TIS-100 Simulator/StackMemoryNode.cpp
@@ -3,3 +3,40 @@
 #include "StackMemoryNode.h"
 #include "IOChannel.h"
 
+StackMemoryNode::StackMemoryNode()
+{}
+
+void StackMemoryNode::SetNeighbor(Neighbor direction, std::shared_ptr<IOChannel>& spIO)
+{
+    //TODO
+}
+
+void StackMemoryNode::Initialize()
+{
+    m_data.clear();
+}
+
+void StackMemoryNode::Read()
+{
+    //TODO
+}
+
+void StackMemoryNode::Compute()
+{
+    //TODO
+}
+
+void StackMemoryNode::Write()
+{
+    //TODO
+}
+
+void StackMemoryNode::WriteComplete()
+{
+    //TODO
+}
+
+void StackMemoryNode::Step()
+{
+    //TODO
+}
